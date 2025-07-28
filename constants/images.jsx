9636a682c30756f137add0fa7a59eba892b55157
Anshulyuvmedia <anshulyuvmedia@gmail.com>
2025-07-28 17:48:31 +0530

    wokring package

--- constants/images.jsx
+++ constants/images.jsx
@@ -1,7 +1,7 @@
 import ironbutterfly from "@/assets/images/IronButterfly.png";
 import shortstraddle from "@/assets/images/ShortStraddle.png";
 import shortstrangle from "@/assets/images/ShortStrangle.png";
-import mainlogo from "@/assets/images/aianalyzerlogo.png";
+import mainlogo from "@/assets/images/ailogo.png";
 import courseimg from "@/assets/images/courseimg.webp";
 import rupee from "@/assets/images/rupee.png";
 
